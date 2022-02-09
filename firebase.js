4f4179914ec4f5caa28db05d805c5b162c2c1dc0
DylanLev <dylan.levy5@yahoo.com>
2022-02-09 14:54:12 +0100

    Summary
    Starting Files

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDhC29KK2L13HJQEbS1syHXpEJwaJkv_D0",
+    authDomain: "website-4e5d9.firebaseapp.com",
+    projectId: "website-4e5d9",
+    storageBucket: "website-4e5d9.appspot.com",
+    messagingSenderId: "66589310606",
+    appId: "1:66589310606:web:d2a622082a99286d5893df",
+    measurementId: "G-Y3GBZ5N4Z3"
+  };
