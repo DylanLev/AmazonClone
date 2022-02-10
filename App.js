07ba368bc2705114a089c6c9adf81709faea963d
DylanLev <dylan.levy5@yahoo.com>
2022-02-10 13:41:17 +0100

    Summary
    Header search bar, search icon

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM
-    <div className="App">
-     <h1>Hello</h1>
+    <div className="app">
+     <Header />
     </div>
   );
 }
